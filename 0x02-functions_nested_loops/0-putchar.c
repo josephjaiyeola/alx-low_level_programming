7b67927e1e3457e52db10746bfb9170b56fb1d88
joseph <jaiyejoseph2015@gmail.com>
2023-02-21 13:42:07 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
- * main - Program that prints _putchar, followed by a new line
- * Return: Always 0
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
